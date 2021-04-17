f4b924dbed9568cf1d9e8129a3f8d750e46ce269
Raf Swiderski <rafswid@icloud.com>
2021-04-17 22:41:20 +0100

    register and login forms working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,36 @@
 import '../css/Authentication.css'
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function LogIn() {
-    let history = useHistory();
+    // let history = useHistory();
     
+    //Defining constants, when the user fills in the form, we assign the user's input values to these.
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    function submitForm() {
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+    }
     
     return (
-      <div class="sign-in-container">
-        <div class="sign-in-form-container">
+      <div>
+        <div>
           <h1>Login Page</h1>
-          <form class="sign-in-form">
-            <input name="username" /><br></br>
-            <input name="password" />
+          <form onSubmit={submitForm}>
+          <div>
+              <input type="text" id="username" name="username" placeholder="johnsmith" 
+              onChange={({ target }) => setUsername(target.value)} required/>
+          </div>
+          <div>
+              <input type="password" id="password" name="password" placeholder="password" 
+              onChange={({ target }) => setPassword(target.value)} required/>
+          </div>
+          <input type="submit" value="Log In"/>
           </form>
-          <button 
-            onClick={() => {
-              history.push('/home');
-            }}>Log In
-          </button>
+
           <div>If you don't have an  account, please register.</div>
           <Link to="/register">Register</Link>
           <ul>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,21 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import { useState } from "react";
 
 function Register() {
-    let history = useHistory();
 
+    // let history = useHistory(); -- This object lets you send the user to different pages. 
+    // Used like this, in an html tag: onClick={() => {history.push('/home'); }}
+
+    //Defining constants, when the user fills in the form, we assign the user's input values to these.
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [password_confirmation, setPasswordConfirmation] = useState("")
 
+    //function that assigns the form values to the browser's storage. (like sessions/cookies)
+    //Local storage object can be see when you type `console.log(localStorage)` into the console
     function submitForm() {
         localStorage.setItem('name', name);
         localStorage.setItem('email', email);
         localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+        localStorage.setItem('password_confirmation', password_confirmation);
     }
 
     return (
@@ -23,11 +30,12 @@ function Register() {
             <title>Register</title>  
             <h1>Create An Account</h1><br></br>
 
-            <div className="sign-in-container">
-                <div className="form-container">
+            <div>
+                <div>
                 <form onSubmit={submitForm}>
                     <div>
-                        <input type="text" id="name" name="name" placeholder="Name" onChange={({ target }) => setName(target.value)} required/>
+                        <input type="text" id="name" name="name" placeholder="Name" 
+                        onChange={({ target }) => setName(target.value)} required/>
                     </div>
                     <div>
                         <input type="text" id="email" name="email" placeholder="john@smith.com" 
@@ -38,13 +46,15 @@ function Register() {
                         onChange={({ target }) => setUsername(target.value)} required/>
                     </div>
                     <div>
-                        <input type="password" id="password" name="password" placeholder="password" onChange={({ target }) => setPassword(target.value)} required/>
+                        <input type="password" id="password" name="password" placeholder="password" 
+                        onChange={({ target }) => setPassword(target.value)} required/>
                     </div>
                     <div>
-                        <input type="password" id="password_confirmation" name="password_confirmation" placeholder="re-type password"  onChange={({ target }) => setPasswordConfirmation(target.value)} required/>
+                        <input type="password" id="password_confirmation" name="password_confirmation" placeholder="re-type password"  
+                        onChange={({ target }) => setPasswordConfirmation(target.value)} required/>
                     </div>
                     <div>
-                        <input type="submit" value="Create Account" onClick={() => {history.push('/home'); }}/>
+                        <button type="submit" value="Create Account" >Submit</button>
                     </div>
                 </form>
 
