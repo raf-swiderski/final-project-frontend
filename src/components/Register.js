8b02c258d48e4278b5e528520dcfb28a3b3abdaa
Raf Swiderski <rafswid@icloud.com>
2021-04-17 21:46:54 +0100

    storing username etc on Localstorage

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
+import { useState } from "react";
 
 function Register() {
     let history = useHistory();
@@ -11,36 +12,41 @@ function Register() {
     const [password, setPassword] = useState("")
     const [password_confirmation, setPasswordConfirmation] = useState("")
 
+    function submitForm() {
+        localStorage.setItem('name', name);
+        localStorage.setItem('email', email);
+        localStorage.setItem('username', username);
+    }
+
     return (
         <div>
             <title>Register</title>  
             <h1>Create An Account</h1><br></br>
 
-            <div class="sign-in-container">
-                <div class="form-container">
-                <form action="/users/register" method="POST">
+            <div className="sign-in-container">
+                <div className="form-container">
+                <form onSubmit={submitForm}>
                     <div>
-                        <input type="text" id="name" name="name" placeholder="Name" required/>
+                        <input type="text" id="name" name="name" placeholder="Name" onChange={({ target }) => setName(target.value)} required/>
                     </div>
                     <div>
-                        <input type="text" id="email" name="email" placeholder="john@smith.com" required/>
+                        <input type="text" id="email" name="email" placeholder="john@smith.com" 
+                        onChange={({ target }) => setEmail(target.value)} required/>
                     </div>
                     <div>
-                        <input type="text" id="username" name="username" placeholder="@john_smith" required/>
+                        <input type="text" id="username" name="username" placeholder="johnsmith" 
+                        onChange={({ target }) => setUsername(target.value)} required/>
                     </div>
                     <div>
-                        <input type="password" id="password" name="password" placeholder="password" required/>
+                        <input type="password" id="password" name="password" placeholder="password" onChange={({ target }) => setPassword(target.value)} required/>
                     </div>
                     <div>
-                        <input type="password" id="password_confirmation" name="password_confirmation" placeholder="re-type password" required/>
+                        <input type="password" id="password_confirmation" name="password_confirmation" placeholder="re-type password"  onChange={({ target }) => setPasswordConfirmation(target.value)} required/>
                     </div>
                     <div>
-                        <input type="submit" value="Create Account"/>
+                        <input type="submit" value="Create Account" onClick={() => {history.push('/home'); }}/>
                     </div>
-                    <a href="/users/login">Already have an account? Log in here!</a>
                 </form>
-
-                <button onClick={() => {history.push('/home'); }}>Register</button>
 
                 <Link to="/login">Log In</Link>
 
