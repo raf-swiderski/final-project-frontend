2c844d9f14d44cac4c8a5a6e14657b1b0f3cea0f
sandy <pounoussamy.sandy@gmail.com>
2021-04-20 19:13:47 +0100

    Added click function to button, triggering a request to server

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,10 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import clickRecipe from './ShowRecipe';
 
 // this is a function(component) which takes one argument-->props=properties (array of recipes)
 function RecipeList(props) {
   console.log(props)
+  const recipe_id = props.recipes.recipe_id
+
+  const clickRecipe =  async e => {
+    e.preventDefault();
+    try {
+      const recipeSteps = await fetch(
+        `http://localhost:9000/recipe`,
+        {
+          method: 'GET',
+          headers: { "Content-Type": "application/json",
+          recipe_id: JSON.stringify(recipe_id) 
+          }
+  
+        }
+      )
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
+  
   return (
       <table>
          <thead>
@@ -17,15 +36,13 @@ function RecipeList(props) {
       {props.recipes.map((recipe) => (
          <tr key = {recipe.id.toString()}>
           <td>{recipe.recipe_name}</td>
-          <td><button data-target={`${recipe.recipe_id}`}>Train</button></td>
+          <td><button onClick={clickRecipe}>Show</button></td>
         </tr>
       ))}
       </tbody>
     </table>
   );
 }
-
-
 
 // export recipeList component so that can be imported in other places
 export default RecipeList;
