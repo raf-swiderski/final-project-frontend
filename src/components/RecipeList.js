f99702f365e306513b52cedf66efc93962719f56
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-04-21 15:43:24 +0100

    removed comment

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,28 +3,6 @@ import { Link } from "react-router-dom";
 
 // this is a function(component) which takes one argument-->props=properties (array of recipes)
 function RecipeList({ recipes }) {
-  console.log(recipes);
-
-  //const clickRecipe = async (e) => {
-  //   e.preventDefault();
-  //   // const recipe_id = parseInt(e.currentTarget.value);
-  //   // try {
-  //   //   const data = await fetch(
-  //   //     `http://localhost:9000/recipe?recipe_id=${recipe_id}`,
-  //   //     {
-  //   //       method: "GET",
-  //   //       headers: { "Content-Type": "application/json" },
-  //   //     }
-  //   //   );
-  //   //   const recipeData = await data.json();
-  //   //   console.log(recipeData[0].steps); // this is my recipe steps array!
-  //   //   const recipeSteps = recipeData[0].steps;
-  //   // } catch (err) {
-  //   //   console.error(err.message);
-  //   // }
-  //   // //redirect
-  //};
-
   return (
     <table>
       <thead>
