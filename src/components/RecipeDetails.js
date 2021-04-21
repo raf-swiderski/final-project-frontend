f66aed275e08fbfa5c1aa8bc7cbee968f51228e9
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-04-21 14:47:39 +0100

    Add recipePage

--- src/components/RecipeDetails.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useHistory, useParams } from "react-router-dom";
-
-const RecipePage = () => {
-  return <h1>Recipe Detailed Instruction</h1>
-}
-
-export default RecipePage;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -2,52 +2,47 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 // this is a function(component) which takes one argument-->props=properties (array of recipes)
-function RecipeList({recipes}) {
-  console.log(recipes)
+function RecipeList({ recipes }) {
+  console.log(recipes);
 
-  const clickRecipe =  async (e) => {
-    // e.preventDefault();
+  //const clickRecipe = async (e) => {
+  //   e.preventDefault();
+  //   // const recipe_id = parseInt(e.currentTarget.value);
+  //   // try {
+  //   //   const data = await fetch(
+  //   //     `http://localhost:9000/recipe?recipe_id=${recipe_id}`,
+  //   //     {
+  //   //       method: "GET",
+  //   //       headers: { "Content-Type": "application/json" },
+  //   //     }
+  //   //   );
+  //   //   const recipeData = await data.json();
+  //   //   console.log(recipeData[0].steps); // this is my recipe steps array!
+  //   //   const recipeSteps = recipeData[0].steps;
+  //   // } catch (err) {
+  //   //   console.error(err.message);
+  //   // }
+  //   // //redirect
+  //};
 
-    const recipe_id = parseInt(e.currentTarget.value)
-
-    try {
-      const data = await fetch(
-        `http://localhost:9000/recipe?recipe_id=${recipe_id}`,
-        {
-          method: 'GET',
-          headers: { "Content-Type": "application/json",
-          }
-        }
-      ) 
-    const recipeData = await data.json();
-    console.log(recipeData[0].steps) // this is my recipe steps array!
-    const recipeSteps = recipeData[0].steps;
-    } catch (err) {
-      console.error(err.message)
-    }
-    //redirect
-  }
-  
   return (
-      <table>
-         <thead>
-          <tr>
-            <th>Recipes</th>
-            <th>Train</th>
-          </tr>
-        </thead>
-      <tbody>
-      {recipes.map((recipe) => (
-         <tr key = {recipe.id.toString()}>
-          <td>{recipe.id}</td>
-          <td>{recipe.recipe_name}</td>
-          <td><Link to={`/recipe/${recipe.id}`}>
-            <button value={recipe.id} onClick={clickRecipe}>
-              Show
-            </button>
-          </Link></td>
+    <table>
+      <thead>
+        <tr>
+          <th>Recipes</th>
+          <th>Train</th>
         </tr>
-      ))}
+      </thead>
+      <tbody>
+        {recipes.map((recipe) => (
+          <tr key={recipe.id.toString()}>
+            <td>{recipe.id}</td>
+            <td>{recipe.recipe_name}</td>
+            <td>
+              <Link to={`/recipe/${recipe.recipe_id}`}> Show</Link>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
@@ -55,5 +50,3 @@ function RecipeList({recipes}) {
 
 // export recipeList component so that can be imported in other places
 export default RecipeList;
-
-
