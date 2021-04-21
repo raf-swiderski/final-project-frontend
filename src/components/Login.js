c8bd252ed749b7a02b2712db85505aad3ae6718f
rhc07 <rorieclarke@gmail.com>
2021-04-21 15:32:12 +0100

    login errors work

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,9 @@ import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
+const UNAUTHORIZED_ERROR = "Unauthorized";
+const INTERNAL_SERVER_ERROR = "Internal Server Error"
+
 function LogIn() {
     const historyHook = useHistory();
 
@@ -23,11 +26,22 @@ function LogIn() {
 
       fetch("http://localhost:9000/users/login", options)
         // turn api response into json
-        .then((res) => res.json())
+        .then((res) => { 
+          // Is the status code 200, 201, 204 , etc ? aka GOOD
+          if(res.ok) {
+            return res.json()
+          }
+
+          if(res.status === 401) {
+            throw new Error(UNAUTHORIZED_ERROR)
+          }
+
+          throw new Error(INTERNAL_SERVER_ERROR)
+        })
         .then((result) => {
-          if (result.errors) {
+          if (result.errors) {            
             // do something about errors
-            setErrors(result.errors);
+            setErrors(result.data.errors);
             return;
           }
 
@@ -41,7 +55,26 @@ function LogIn() {
           historyHook.push('/home')
         })
         .catch((err) => {
-          console.log(err);
+          if(!err.message) {
+            // Uh oh ... we don't know what this is
+            console.log("Mystery Error ... probably just do nothing")
+
+            setErrors( [ { message: "Something went wrong ! Sorry ... try again." } ] );
+          }
+
+          else if(err.message === INTERNAL_SERVER_ERROR){
+            // Show internal server error
+            console.log("Got an internal server error")
+
+            setErrors( [ { message: "Internal Server Error" } ] );
+          }
+
+          else if(err.message === UNAUTHORIZED_ERROR) {
+            // Show wrong username / password error
+            console.log("Got an unauthorized error")
+
+            setErrors( [ { message: "Wrong username or password"} ] );
+          }
         });
 
     }
@@ -50,6 +83,15 @@ function LogIn() {
       <div>
         <div>
           <h1>Login Page</h1>
+          
+          {errors && (
+            <div>
+              {errors.map((error, index) => (
+                <p key={`error-${index}`}>{error.message}</p>
+              ))}
+            </div>
+          )}
+         
           <form onSubmit={submitForm}>
           <div>
               <input type="text" id="email" name="email" placeholder="john@smith.com"
